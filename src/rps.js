f37e35e5e845b238d8bf001813c682f42dd233a4
seba3510 <seba3510@protonmail.com>
2023-12-25 14:46:12 -0400

    Add getComputerChoice function for computer's random play

--- src/rps.js
+++ src/rps.js
@@ -4,3 +4,41 @@
  * @author Sebastian Corporan Berrios
  */
 
+
+// Holds all possible choices
+const arr = ["Rock", "Paper", "Scissors"];
+
+/**
+ * This function generates a random choice for the computer in a Rock, Paper, Scissors game.
+ * @returns {string} The computer's random choice ("ROCK", "PAPER", or "SCISSORS").
+ */
+function getComputerChoice() {
+    // Get the length of the array representing the game choices
+    let n = arr.length;
+
+    // Initialize the lower and upper bounds for generating a random index
+    let lo = 0;
+    let hi = n - 1;
+
+    // Calculate the difference between upper and lower bounds
+    const diff = hi - lo;
+
+    // Generate a random multiplier between 0 (inclusive) and the difference
+    const mult = Math.random() * diff;
+
+    // Calculate the final random value within the specified range
+    let val = mult + lo;
+
+    // Determine the computer's choice based on the random value
+    // Convert the choice to uppercase for consistency
+    switch ((val)) {
+        case 0: // Computer chose "Rock"
+            return arr[0].toUpperCase();
+
+        case 1: // Computer chose "Paper"
+            return arr[1].toUpperCase();
+
+        case 2: // Computer chose "Scissors"
+            return arr[2].toUpperCase();
+    } // switch()
+} // getComputerChoice
